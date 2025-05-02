e0d72af395597123fbe14a8b796d78bf04ed55bc
Muhammad Ridhwan Khalid <m.ridhwankhalid@gmail.com>
2025-05-02 18:14:49 +1000

    auth backend is added
    started doing the income backend

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,24 @@
+
+const User = require("../models/User");
+const Income = require("../models/Income");
+
+
+// Add Income Source
+exports.addIncome = async (req, res) => {
+    
+}
+
+//Get All Income
+exports.getAllIncome = async (req, res) => {
+    
+}
+
+//Download Excel
+exports.downloadIncomeExcel = async (req, res) => {
+    
+}
+
+//Delete Income Source
+exports.deleteIncome = async (req, res) => {
+    
+}
